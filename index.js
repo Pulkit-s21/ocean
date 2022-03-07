43b7987d00d36c509f7926249058b06a922964cd
Pulkit-s21 <pulkitsaxena795@gmail.com>
2022-03-07 20:10:49 +0530

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const trashContainer = document.querySelector(".trash-container");
 const moneyElem = document.querySelector(".money");
-const currentFormatter = new Intl.NumberFormat("en-us", {
+const currencyFormatter = new Intl.NumberFormat("en-us", {
     style: "currency",
     currency: "USD",
     maximumFractionDigits: 0
@@ -16,10 +16,12 @@ const MAX_AMOUNT_RAISED = 50000000;
 
 setupTrash();
 async function setupTrash() {
+    // getting the donation money value from the Team Seas site
     const amountRaised = await fetch("https://tscache.com/donation_total.json").then(res => res.json()).then(data => data.count);
     
-    moneyElem.innerText = currentFormatter.format(amountRaised);
+    moneyElem.innerText = currencyFormatter.format(amountRaised);
 
+    // using Math.max() cz we dont want the site to show negative trash once the amount raised crossed max value so we set that to 0
     const amountLeftToBeRaised = Math.max(MAX_AMOUNT_RAISED - amountRaised, 0);
 
     const stringifiedAmount = trashFormatter.format(amountLeftToBeRaised);
@@ -28,6 +30,7 @@ async function setupTrash() {
             amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
             icon: "bag",
         },
+        // we dont use the `` from here on because we only use 1 digit at each place
         xl: {
             amount: parseInt(stringifiedAmount[2]),
             icon: "takeout",
@@ -49,6 +52,7 @@ async function setupTrash() {
             icon: "bottle",
         },
     }
+    // I need to understand this in depth
     Object.values(trashAmount).forEach(({ amount, icon }) => {
         for (let i = 0; i < amount; i++) {
             createTrash(icon);
@@ -58,14 +62,14 @@ async function setupTrash() {
 
 function createTrash(icon) {
     const img = document.createElement("img");
-    const top = randomNumberBetween(0, 50);
+    const top = randomNumberBetween(0, 50); // range is from 0 to 50 cz the ocean is till 50vh of the screen
     const size = top / 5 + 1;
     img.classList.add("trash");
     img.src = `/imgs/${icon}.svg`;
     img.style.width = `${size}vmin`;
     img.style.height = `${size}vmin`;
     img.style.top = `${top}vh`;
-    img.style.left = `${randomNumberBetween(0,100)}vw`;
+    img.style.left = `${randomNumberBetween(0,100)}vw`; // randomly select the position horizontally
     img.style.setProperty("--rotation", `${randomNumberBetween(-30, 30)}deg`)
     trashContainer.appendChild(img);
 }
